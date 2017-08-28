077145e4e947a475e097c1596d81733f297ed899
Jukka Juslin <jukka.juslin@haaga-helia.fi>
2017-08-28 13:05:02 +0300

    tyylit ulkoisesta css tiedostosta

--- 1-react-basics/src/components/header.js
+++ 1-react-basics/src/components/header.js
@@ -2,20 +2,9 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    const styles = {
-      header: {
-        background: "#03a9f4"
-      },
-      logo: {
-        color: "#fff",
-        fontFamily: "Anton",
-        textAlign: "center"
-      }
-    };
-
     return (
-      <header style={styles.header}>
-        <div style={styles.logo}>Logo</div>
+      <header>
+        <div className="logo">Logo</div>
         <input />
       </header>
     );
